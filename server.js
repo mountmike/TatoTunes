bebfba98cdb17b9698a3c2f90d4a8dc9d130faec
Michael <tharratt.michael@gmail.com>
2023-03-14 12:28:50 +1100

    configurating boiletplate and middleware

--- server.js
+++ server.js
@@ -2,21 +2,36 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8888;
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("./middlewares/method_override");
+const setCurrentUser = require("./middlewares/set_current_user");
+const viewHelpers = require("./middlewares/view_helpers");
+const session = require("express-session");
+const MemoryStore = require('memorystore')(session);
+const db = require("./db");
 
-const methodOverride = require("./middlewares/method_override")
-const setCurrentUser = require("./middlewares/setCurrentUser")
+// configs
+app.set("view engine", "ejs");
+app.set("layout layout_login", false);
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride);
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    secret: process.env.SESSION_SECRET || 'Godogsgo',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(setCurrentUser);
+app.use(viewHelpers);
 
-const dishController = require("./controllers/dish_controller")
-const userController = require("./controllers/user_controller")
-const sessionController = require("./controllers/session_controller")
 
-const session = require("express-session")
-const MemoryStore = require('memorystore')(session)
-const db = require("./db")
-
-
-
-
+app.get("/", (req, res) => {
+    res.send("good")
+})
 
 
 
