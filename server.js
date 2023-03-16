78e53f1f64fc8da0db66e045c8acc87c2bd8656a
Michael <tharratt.michael@gmail.com>
2023-03-16 16:19:01 +1100

    updated env variables for deployment

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session({
     pool : db,                // Connection pool
     tableName: "session"
   }),
-  secret: "go dogs god",
+  secret: process.env.SESSION_SECRET || "godogsgo",
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
